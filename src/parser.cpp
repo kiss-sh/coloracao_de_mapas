092b74746d2fe83fda0b44fbd6befc5371174011
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-05-06 16:40:26 -0300

    parser de arquivo pronto

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
+#include "mapa.hpp"
 #include <fstream>
 #include <string>
 #include <vector>
 using std::ifstream;
 using std::string;
 using std::vector;
-using std::cout;
-using std::endl;
 
 string ler_nome_vertice(ifstream &arquivo) {
   vector<char> variavel_char;
@@ -19,22 +17,27 @@ string ler_nome_vertice(ifstream &arquivo) {
     }
   }
   string nome_vertice(variavel_char.begin(), variavel_char.end());
-  cout << "teste: " << nome_vertice << endl;
   return nome_vertice;
 }
 
-void ler_arquivo(string filename) {
-  string nome_v1;
+vector<Vertice*> ler_arquivo(string filename) {
+  Grafo grafo;
+  string nome_vertice, nome_adjacente;
+  bool in_braket = false;
+
   char c;
   ifstream arquivo(filename);
   while (arquivo.get(c)) {
-    if (c == '"') {
-      nome_v1 = ler_nome_vertice(arquivo);
+    if (c == '"' && in_braket == false) {
+      nome_vertice = ler_nome_vertice(arquivo);
+
+    } else if (c == '"' && in_braket == true) {
+      nome_adjacente = ler_nome_vertice(arquivo);
+      grafo.add(nome_vertice, nome_adjacente);
+
+    } else if (c == '[' || c == ']') {
+      in_braket = !in_braket;
     }
   }
-}
-
-// teste
-int main(int argc, char const *argv[]) {
-  ler_arquivo(argv[1]);
+  return grafo.vertices;
 }
