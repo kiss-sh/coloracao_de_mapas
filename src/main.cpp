f06c87465f7d979fde23134409938d41b2a40c02
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-05-13 23:16:21 -0300

    codigo em andamento, codigo inicial que desenha dois vertices

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,40 @@ using std::endl;
 using std::string;
 
 extern vector<VerticePosition*> global_var_vertices;
+extern vector<ArestaPosition*>  global_var_arestas;
 
 int main(int argc, char const *argv[]) {
-  if (argc == 3 && argv[1][0] == '-' && argv[1][1] == 'f') {
-    string filename = argv[2];
+  if (argc == 2) {
+    string filename = argv[1];
 
     cout << "lendo o arquivo ..." << endl; // log
     vector<Vertice*> vertices = ler_arquivo(filename);
     cout << "leitura finalizada" << endl; // log
 
     if (vertices.size() == 2) {
+      // vertices
       VerticePosition *v1 = new VerticePosition;
+      v1->x = 100;
+      v1->y = 100;
+      VerticePosition *v2 = new VerticePosition;
+      v2->x = 200;
+      v2->y = 100;
 
       global_var_vertices.push_back(v1);
+      global_var_vertices.push_back(v2);
+
+      // arestas
+      ArestaPosition *a1 = new ArestaPosition;
+      a1->x_begin = v1->x;
+      a1->y_begin = v1->y;
+      a1->x_end = v2->x;
+      a1->y_end = v2->y;
+
+      global_var_arestas.push_back(a1);
     }
   }
 
+  open_window(800, 400);
 
   return 0;
 }
