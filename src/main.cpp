a4f2cf888c8d7cf244b2ce5f6e45d58cf47d9276
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-05-15 00:26:03 -0300

    implementacao do desenho do grafo

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
+#include <map>
 #include <string>
+#include <vector>
 #include "gl_wrapper.hpp"
 #include "mapa.hpp"
 #include "parser.hpp"
 using std::cout;
 using std::endl;
+using std::map;
 using std::string;
 
 extern vector<VerticePosition*> global_var_vertices;
 extern vector<ArestaPosition*>  global_var_arestas;
+
+VerticePosition *obterPointer(string nome) {
+  for (int it = 0; it < global_var_vertices.size(); it++) {
+    if(global_var_vertices[it]->nome == nome) {
+      return global_var_vertices[it];
+    }
+  }
+}
+
+void montarArestas(vector<Vertice*> vertices) {
+  for (int x = 0; x != vertices.size(); x++) {
+    VerticePosition *v = obterPointer(vertices[x]->nome);
+
+    map<string, Vertice*>::iterator it;
+    for (it = vertices[x]->adjacentes.begin(); it != vertices[x]->adjacentes.end(); it++) {
+      VerticePosition *v_adjacente = obterPointer(it->first);
+      ArestaPosition *aresta = new ArestaPosition;
+      aresta->x_begin = v->x;
+      aresta->y_begin = v->y;
+      aresta->x_end = v_adjacente->x;
+      aresta->y_end = v_adjacente->y;
+      global_var_arestas.push_back(aresta);
+    }
+  }
+}
 
 int main(int argc, char const *argv[]) {
   if (argc == 2) {
@@ -32,7 +60,7 @@ int main(int argc, char const *argv[]) {
           v->nome = vertices[index]->nome;
           global_var_vertices.push_back(v);
 
-          x_axis += 50;
+          x_axis += 100;
           index++;
 
         } else {
@@ -45,9 +73,11 @@ int main(int argc, char const *argv[]) {
         break;
       } else {
         x_axis = 100;
-        y_axis += 50;
+        y_axis += 100;
       }
     }
+
+    montarArestas(vertices);
   }
 
   open_window(800, 400);
